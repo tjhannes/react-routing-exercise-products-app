03f547527df4e9dcc9bedfd1edff96e2de7cd4ec
tjhannes <johannes_thanner@live.de>
2024-07-09 10:51:38 +0200

    add warenkorb

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,17 +2,21 @@ import { Link } from "react-router-dom";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import ProductInfo from "./ProductInfo";
 
-export default function ProductCard({ product }) {
+export default function ProductCard({ product, setWarenkorb }) {
   return (
-    <Link to={"/product/" + product.id}>
-      <div className="card">
-        <ProductInfo product={product} />
+    <>
+      <Link to={"/product/" + product.id}>
+        <div className="card">
+          <ProductInfo product={product} />
+        </div>
+      </Link>
+      <button onClick={() => setWarenkorb((prev) => [...prev, product])}>
         <div className="button-add-to-cart">
           <AddCircleOutlineIcon />
           Zum Warenkorb hinzufügen!
         </div>
-        <hr />
-      </div>
-    </Link>
+      </button>
+      <hr />
+    </>
   );
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 
 export default function ProductList({ apiEndpoint }) {
   const [products, setProducts] = useState([]);
+  const [warenkorb, setWarenkorb] = useState([]);
+  const [price, setPrice] = useState(0);
 
   useEffect(() => {
     axios
@@ -15,10 +17,16 @@ export default function ProductList({ apiEndpoint }) {
 
   return (
     <>
+      <p>Warenkorb Anzahl Produkte: {warenkorb.length}</p>
+      <p>Preis: </p>
       {products.map((product) => (
         /* The code is used both at ProductMen and ProductWomen */
         /* Refactor this code into a component */
-        <ProductCard product={product} key={product.id} />
+        <ProductCard
+          product={product}
+          key={product.id}
+          setWarenkorb={setWarenkorb}
+        />
       ))}
     </>
   );
